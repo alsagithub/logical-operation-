327672bbb7e6bc8e687821d416aebf1b647e8d49
Samia <162901845+alsagithub@users.noreply.github.com>
2024-03-15 21:00:33 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+// AND OPERATERS---
+//let a:number=3;
+//let b:number=4;
+//console.log((a<b)&&(b>a));
+
+
+//OR OPERATEERS----
+//let a:number=5;
+//let b:number=9;
+//console.log((a>b)||(b<a));
+
+
+//NOR OPERATERS---
+//let a:number=9;
+//let b:number=20;
+//console.log(!(a==b));
